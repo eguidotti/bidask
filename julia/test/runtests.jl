ffbdb7122c305050df6dcbefa8af264aa77eb1b9
Tyler Thomas <tyler@saguarocm.com>
2022-08-17 12:42:08 -0700

    remove the test data from the julia folder

--- julia/test/runtests.jl
+++ julia/test/runtests.jl
@@ -4,7 +4,7 @@ using DataFrames
 using Test
 
 @testset "edge()" begin
-    df = CSV.File("./ohlc.csv")
+    df = CSV.File("../../pseudocode/ohlc.csv")
     estimate = edge(df.:Open, df.:High, df.:Low, df.:Close)
     println(estimate)
     @test isapprox(0.00010101064175748407, estimate)
