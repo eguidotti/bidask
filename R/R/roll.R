6fd11f026963a0f2709caf62559f78fb904b4678
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2023-08-12 17:34:06 +0200

    Update roll.R

--- R/R/roll.R
+++ R/R/roll.R
@@ -4,19 +4,19 @@
 #'
 ROLL <- function(x, width = nrow(x), signed = FALSE, na.rm = FALSE){
 
-  R1 <- x$CLOSE/lag(x$CLOSE, 1) - 1
-  R2 <- lag(R1, 1)
-
-  R1 <- R1[-c(1:2)]
-  R2 <- R2[-c(1:2)]
-
-  E1 <- rmean(R1, width = width-2, na.rm = na.rm)
-  E2 <- rmean(R2, width = width-2, na.rm = na.rm)
-  E12 <- rmean(R1*R2, width = width-2, na.rm = na.rm)
-
-  N <- rsum(!is.na(R2), width = width-2)
+  x <- log(x)
   
-  S2 <- N / (N-1) * 4 * (E1*E2-E12)
+  C <- x$CLOSE
+  C1 <- lag(C, 1)
+  C2 <- lag(C, 2)
+  
+  R1 <- C - C1
+  R2 <- C1 - C2
+  
+  m <- rmean(cbind(R1, R2, R1*R2)[-(1:2),], width = width-2, na.rm = na.rm)
+  n <- rsum(!is.na(R2)[-(1:2),,drop=FALSE], width = width-2)
+  
+  S2 <- -4 * n/(n-1) * (m[,3] - m[,1]*m[,2])
   colnames(S2) <- "ROLL"
   
   S <- sign(S2) * sqrt(abs(S2))
