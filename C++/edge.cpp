6ac2651ef3bb6d21b6ada32344f275dc37038b7b
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2023-09-05 13:16:27 +0200

    Update edge.cpp

--- C++/edge.cpp
+++ C++/edge.cpp
@@ -4,12 +4,21 @@
 #include <stdexcept>
 
 
+template <typename T>
+double mean(const std::vector<T> &x){
+  unsigned int n = x.size(); double sum = 0.0;
+  for(unsigned int i=0; i<n; i++) sum += x[i];
+  return sum / n;
+}
+
+
 double edge(
     const std::vector<double> &open,
     const std::vector<double> &high,
     const std::vector<double> &low,
-    const std::vector<double> &close){
-  
+    const std::vector<double> &close,
+    const bool sign){
+
   unsigned int n = open.size();
   if(high.size() != n or low.size() != n or close.size() != n){
     throw std::invalid_argument(
@@ -26,66 +35,47 @@ double edge(
     m[i] = (h[i] + l[i]) / 2.;
   }
   
-  int x1_n=0, x2_n=0, n1_n=0, n2_n=0, n3_n=0, n4_n=0, n5_n=0;
-  double x1_s=0, x2_s=0, n1_s=0, n2_s=0, n3_s=0, n4_s=0, n5_s=0;
-  double x1_ss=0, x2_ss=0;
-  
-  double tmp;
-  for(unsigned int i=1; i<n; i++){
-    if(std::isfinite(m[i]) and std::isfinite(o[i]) and 
-         std::isfinite(m[i-1]) and std::isfinite(c[i-1])){
-      
-      tmp = (m[i] - o[i]) * (o[i] - m[i-1]) + (m[i] - c[i-1]) * (c[i-1] - m[i-1]);
-      x1_s += tmp;
-      x1_ss += tmp * tmp;
-      x1_n++;  
-      
-      tmp = (m[i] - o[i]) * (o[i] - c[i-1]) + (o[i] - c[i-1]) * (c[i-1] - m[i-1]);
-      x2_s += tmp;
-      x2_ss += tmp * tmp;
-      x2_n++;  
-      
-      n1_s += int(o[i] == h[i]);
-      n1_n++;  
-      
-      n2_s += int(o[i] == l[i]);
-      n2_n++;  
-      
-      n3_s += int(c[i-1] == h[i-1]);
-      n3_n++;  
-      
-      n4_s += int(c[i-1] == l[i-1]);
-      n4_n++;  
-      
-      n5_s += int(h[i] == l[i] and l[i] == c[i-1]);
-      n5_n++;  
-      
-    }
+  std::vector<unsigned int> tau(n-1), phi1(n-1), phi2(n-1), phi3(n-1), phi4(n-1);
+  std::vector<double> r1(n-1), r2(n-1), r3(n-1), r4(n-1), r5(n-1);
+  for(unsigned int i=0; i<n-1; i++){
+    tau[i] = (h[i+1] != l[i+1]) | (l[i+1] != c[i]);
+    phi1[i] = (o[i+1] != h[i+1]) & tau[i];
+    phi2[i] = (o[i+1] != l[i+1]) & tau[i];
+    phi3[i] = (c[i] != h[i]) & tau[i];
+    phi4[i] = (c[i] != l[i]) & tau[i];
+    r1[i] = m[i+1] - o[i+1];
+    r2[i] = o[i+1] - m[i];
+    r3[i] = m[i+1] - c[i];
+    r4[i] = c[i] - m[i];
+    r5[i] = o[i+1] - c[i];
   }
   
-  double 
-    e1 = x1_s / x1_n,
-    e2 = x2_s / x2_n,
-    
-    v1 = x1_n / (x1_n-1.) * (x1_ss / x1_n - std::pow(x1_s / x1_n, 2)),
-    v2 = x2_n / (x2_n-1.) * (x2_ss / x2_n - std::pow(x2_s / x2_n, 2)),
-    
-    w1 = v2 / (v1 + v2),
-    w2 = v1 / (v1 + v2),
-    k = 4 * w1 * w2,
-    
-    n1 = n1_s / n1_n,
-    n2 = n2_s / n2_n,
-    n3 = n3_s / n3_n,
-    n4 = n4_s / n4_n,
-    n5 = n5_s / n5_n,
-    
-    s2 = -4 * (w1 * e1 + w2 * e2) / ((1 - k * (n1 + n2) / 2) + (1 - n5) * (1 - k * (n3 + n4) / 2));
-  
-  double s=0;
-  if(s2 > 0){
-    s = std::sqrt(s2);
+  double pt = mean(tau);
+  double m1 = mean(r1), m3 = mean(r3), m5 = mean(r5); 
+  std::vector<double> d1(n-1), d3(n-1), d5(n-1);
+  for(unsigned int i=0; i<n-1; i++){
+    d1[i] = r1[i] - tau[i] * m1 / pt;
+    d3[i] = r3[i] - tau[i] * m3 / pt;
+    d5[i] = r5[i] - tau[i] * m5 / pt;
   }
   
-  return(s);
+  double po = mean(phi1) + mean(phi2), pc = mean(phi3) + mean(phi4);
+  std::vector<double> x1(n-1), x2(n-1), x11(n-1), x22(n-1);
+  for(unsigned int i=0; i<n-1; i++){
+    x1[i] = -4./po*d1[i]*r2[i] -4./pc*d3[i]*r4[i];
+    x2[i] = -4./po*d1[i]*r5[i] -4./pc*d5[i]*r4[i];
+    x11[i] = x1[i] * x1[i];
+    x22[i] = x2[i] * x2[i];
+  }
+  
+  double e1 = mean(x1), e2 = mean(x2);
+  double v1 = mean(x11) - e1*e1, v2 = mean(x22) - e2*e2;
+  double s2 = (v2*e1 + v1*e2) / (v1 + v2);
+  double s = std::sqrt(std::abs(s2));
+  
+  if(sign & (s2 < 0))
+    s = -s;
+  
+  return s;
+  
 }
--- C++/edge.h
+++ C++/edge.h
@@ -5,5 +5,5 @@ double edge(
     const std::vector<double> &open,
     const std::vector<double> &high,
     const std::vector<double> &low,
-    const std::vector<double> &close);
-  
+    const std::vector<double> &close,
+    const bool sign = false);
